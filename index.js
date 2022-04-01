9bbd89a110644b175a192a4274b7378fedd60465
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2022-04-01 19:04:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+function createEmployeeRecord(array){
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+
+}
+
